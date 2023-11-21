238f6eefa2babb49ab65576852f900161ee226c6
twlai251 <twlai251@gmail.com>
2023-11-20 23:26:08 -0500

    upload files and progress

--- app/src/main/java/com/example/chitchat/Login.kt
+++ app/src/main/java/com/example/chitchat/Login.kt
@@ -3,8 +3,11 @@ package com.example.chitchat
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
 
@@ -13,9 +16,15 @@ class Login : AppCompatActivity() {
     private lateinit var btnLogin: Button
     private lateinit var btnSignUp: Button
 
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+
+        mAuth = FirebaseAuth.getInstance()
 
         edtEmail =  findViewById(R.id.edt_email)
         edtPassword =  findViewById(R.id.edt_password)
@@ -25,8 +34,31 @@ class Login : AppCompatActivity() {
 
         btnSignUp.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
+            finish()
             startActivity(intent)
         }
 
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email, password)
+        }
+
+    }
+
+    private fun login(email: String, password: String) {
+        // logic of login user
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Logic for user login
+                    val intent = Intent(this@Login, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@Login, "User does not exist", Toast.LENGTH_SHORT).show()
+
+                }
+            }
     }
 }
--- app/src/main/java/com/example/chitchat/MainActivity.kt
+++ app/src/main/java/com/example/chitchat/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.chitchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userRecyclerView: RecyclerView
+    private lateinit var userList: ArrayList<User>
+    private lateinit var adapter: UserAdapter
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        userList = ArrayList()
+        adapter = UserAdapter(this, userList)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.logout) {
+            mAuth.signOut()
+            finish()
+            //logic for logout
+            return true
+        }
+        return true
     }
 }
