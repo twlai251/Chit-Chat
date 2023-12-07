ef1b0e81815a699a3f40917b269d8d2991c59d2a
Teddie Lai <twlai251@gmail.com>
2023-12-07 16:07:27 -0500

    Fix chat activity where chat bubbles appear for send and receiver

--- app/src/main/java/com/example/chitchat/UserAdapter.kt
+++ app/src/main/java/com/example/chitchat/UserAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.chitchat
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,6 +24,13 @@ class UserAdapter(val context: Context, val userList: ArrayList<User>):
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         val currentUser = userList[position]
         holder.textName.text = currentUser.name
+
+        holder.itemView.setOnClickListener{
+            val intent = Intent(context, ChatActivity::class.java)
+            intent.putExtra("name", currentUser.name)
+            intent.putExtra("uid", currentUser.uid)
+            context.startActivity(intent)
+        }
     }
 
     class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
